63b101fd6c3e15b24d8888c7a62b7ee349e989c5
amphillips633 <amphillips633@gmail.com>
2022-08-26 12:25:53 -0400

    Replace game() to function as best 3 of 5 instead of first to 5. Add game() to end of script to call game() when page is reloaded.

--- script.js
+++ script.js
@@ -1,100 +1,88 @@
-function getComputerChoice() {
 
-//returns random whole number between 0-2
-let getRandomNumber = Math.floor(Math.random() * 3);
-
-  if (getRandomNumber == 0) {
-    return "rock";
-  } else if (getRandomNumber == 1) {
-    return "paper";
-  } else if (getRandomNumber == 2) {
-    return "scissors";
-  }
-
-}
 
 // console.log(getComputerChoice());
 
 
-let playerSelection = prompt("Please enter rock, paper, or scissors");
-playerSelection = playerSelection.toLowerCase();
-let computerSelection = getComputerChoice();
 
-let playerWinRound = `You win! ${playerSelection} beats ${computerSelection}`;
-let computerWinRound = `You lose! ${computerSelection} beats ${playerSelection}`;
-let tieRound = `It's a tie! You both selected ${computerSelection}`;
+let playerSelection;
+let computerSelection;
 
 let playerWinGame = `Congratulation, you win!`;
 let computerWinGame = `Sorry, you lose.`;
+let tieGame = `It's a tie!`;
+
+let playerWinRound;
+let computerWinRound;
+
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+
+    //returns random whole number between 0-2
+    let getRandomNumber = Math.floor(Math.random() * 3);
+    
+      if (getRandomNumber == 0) {
+        return "rock";
+      } else if (getRandomNumber == 1) {
+        return "paper";
+      } else if (getRandomNumber == 2) {
+        return "scissors";
+      }
+    
+    }
 
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    playerSelection = prompt("Please enter rock, paper, or scissors");
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+
+    playerWinRound = `You win! ${playerSelection} beats ${computerSelection}`;
+    computerWinRound = `You lose! ${computerSelection} beats ${playerSelection}`;
+    tieRound = `It's a tie! You both selected ${computerSelection}. Try again.`;
+
     if (playerSelection === computerSelection) {
-        return tieRound;
+        return tieRound, computerWinRound = false, playerWinRound = false;
     } else if ( 
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     )  {
-        return playerWinRound;
+        return playerWinRound, computerWinRound = false;
     } else if (
         (playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "rock")
     )  {
-        return computerWinRound;
+        return computerWinRound, playerWinRound = false;
     }
 }
-
-
-// //plays a round of the game until either playScore or computerScore = 5
-// function game() {
-//     if (playerScore < 5 && computerScore < 5) {
-//         playLoop();
-//     }
-// }
-
-
- 
-// function winCondition() {
-//     let playerScore;
-//     let computerScore;
-
-//     } else if (computerScore === 5) {
-//         return computerWinGame;
-//     } else if (playerScore === 5) {
-//         return playerWinGame;
-//     }
-
 
 function game() {
-    for (let playerScore = 0, computerScore = 0; playerScore < 5 && computerScore < 5; scoreIncrement()) {
+
+    for (let i = 0; i < 5; i++) {
         playRound();
-
-        // if (playerWinRound) {
-        //     playerScore ++;
-        //     console.log(`Player: ${playerScore}
-        //     Computer ${computerScore}`);
-        // } else if (computerWinRound) {
-        //     computerScore++;
-        //     console.log(`Player: ${playerScore}
-        //     Computer ${computerScore}`);
-        // }
-
-        function scoreIncrement() {
-            if (playerWinRound) {
-                playerScore++;
-            } else if (computerWinRound) {
-                computerScore++;
-            }
+        if (playerWinRound) {
+            playerScore ++;
+            alert(playerWinRound);
+        } else if (computerWinRound) {
+            computerScore ++, alert(computerWinRound);
+        } else if (tieRound) {
+            i--, alert(tieRound);
         }
         
+        console.log(`Player: ${playerScore}`);
+        console.log(`Computer: ${computerScore}`);
     }
+    if (playerScore > computerScore) {
+        alert(playerWinGame);
+    } else if (computerScore > playerScore) {
+        alert(playerWinGame);
+    } else if (computerScore === playerScore) {
+        alert(tieGame);
+    }
+
 }
 
-console.log(game());
-
-
-//increments playerScore or computerScore depending on who won round
-
-// console.log(playRound(playerSelection, computerSelection))
+game();
